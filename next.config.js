e9863b19365611c72e96bff72569995fa683854d
la360ma-cell <la360.ma@gmail.com>
2026-03-31 19:13:50 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -4,17 +4,20 @@ const withNextIntl = createNextIntlPlugin('./i18n.ts')
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {
-    formats: ['image/avif', 'image/webp'],
-    remotePatterns: [
-      { protocol: 'https', hostname: '**' },
-    ],
-  },
-  experimental: {
-    serverActions: {
-      allowedOrigins: ['localhost:3000', process.env.NEXT_PUBLIC_SITE_URL?.replace('https://', '') || ''],
+    typescript: {
+          ignoreBuildErrors: true,
     },
-  },
+    images: {
+          formats: ['image/avif', 'image/webp'],
+          remotePatterns: [
+            { protocol: 'https', hostname: '**' },
+                ],
+    },
+    experimental: {
+          serverActions: {
+                  allowedOrigins: ['localhost:3000', process.env.NEXT_PUBLIC_SITE_URL?.replace('https://', '') || ''],
+          },
+    },
 }
 
 module.exports = withNextIntl(nextConfig)
